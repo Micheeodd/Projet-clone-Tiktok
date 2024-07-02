030a530f07ad05c88c00d4fcbb8b6d180d7f3546
Bouwa <bouwa@gmail.com>
2024-07-02 09:14:24 +0200

    page du Login implémanté

--- tiktok_tutorial/lib/main.dart
+++ tiktok_tutorial/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tiktok_tutorial/constant.dart';
+import 'package:tiktok_tutorial/constants.dart';
+import 'package:tiktok_tutorial/views/screens/auth/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: backgroundColor ,
       ),
-      home: const Text('Hello world'),
+      home:  LoginScreen(),
     );
   }
 }
