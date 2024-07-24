a3d53fbe7c52350fb7f4ac50b98deed01613a5a7
Dragapsy-Aymane <elghaoutiayman20@gmail.com>
2024-07-25 00:48:36 +0200

    Finalement
    +Bottom Tab Bar
    +Add Video Screen
    +Confirm Video Screen
    +Uploading Video
    +Displaying Videos
    +Add Video Controller

--- tiktok_tutorial/lib/constants.dart
+++ tiktok_tutorial/lib/constants.dart
@@ -2,8 +2,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok_tutorial/controllers/auth_controller.dart';
+import 'package:tiktok_tutorial/views/screens/add_video_screen.dart';
+import 'package:tiktok_tutorial/views/screens/profile_screen.dart';
+import 'package:tiktok_tutorial/views/screens/search_screen.dart';
+import 'package:tiktok_tutorial/views/screens/video_screen.dart';
 
-import 'controllers/auth_controller.dart';
+List pages = [
+  VideoScreen(),
+  Text('Search Screen'),
+  const AddVideoScreen(),
+  Text('Messages Screen'),
+  Text('Profile Screen')
+];
+
 // COLORS
 const backgroundColor = Colors.black;
 var buttonColor = Colors.red[400];
@@ -16,3 +28,11 @@ var firestore = FirebaseFirestore.instance;
 
 // CONTROLLER
 var authController = AuthController.instance;
+
+
+
+// const VideoScreen(),
+// Text('Search Screen'),
+// AddVideoScreen(),
+// Text('Messages Screen'),
+// Text('Profile Screen'),
